be94e4545e11ddca0c3fa7083dfdc49e07080784
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 03:24:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,5 @@
 def square_array(array)
-   array.each {|element| element * element}
-   return new_array
+   puts array.each {|element| element * element}
 end
 array = [1,2,3]
 square_array(array)
